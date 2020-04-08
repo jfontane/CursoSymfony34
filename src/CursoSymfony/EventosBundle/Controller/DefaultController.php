1091312c2c7eeb68f059508936564b4fb78677c2
jfontanellaz <jfontanellaz@gmail.com>
2020-04-08 10:54:53 -0300

    Practico 11 - Entity Consulta Terminada y el controller de consultaAction

--- src/CursoSymfony/EventosBundle/Controller/DefaultController.php
+++ src/CursoSymfony/EventosBundle/Controller/DefaultController.php
@@ -3,17 +3,17 @@
 namespace CursoSymfony\EventosBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints as contraint;
+//use Symfony\Component\Validator\Constraints\Length;
+//use Symfony\Component\Validator\Constraints\NotBlank;
+//use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use CursoSymfony\EventosBundle\Controller\AbstractBaseController;
-
+use CursoSymfony\EventosBundle\Entity\Consulta;
 
 class DefaultController extends Controller {
 
@@ -46,24 +46,50 @@ class DefaultController extends Controller {
 
     public function consultaAction(Request $request) {
         $defaultData = array('consulta' => 'Escriba aquí su consulta ...');
-        $form = $this->createFormBuilder($defaultData)
-                ->add('nombre', TextType::class, array(
-                    'constraints' => new Length(array('min' => 3)),
-                ))
+        $collectionConstraints = new contraint\Collection(array(
+            'nombre' => new contraint\Length(array('min' => 3)),
+            'email' => new contraint\Email(array('message' => 'Dirección de email inválida')),
+        ));
+        $collectionConstraints->allowExtraFields = true;
+        $form = $this->createFormBuilder($defaultData, array('constraints' => $collectionConstraints))
+                ->add('nombre', TextType::class)
                 ->add('email', EmailType::class)
                 ->add('consulta', TextareaType::class)
                 ->add('Enviar', SubmitType::class)
                 ->getForm();
         $form->handleRequest($request);
+        $consulta = new Consulta();
+        $consulta->setConsulta("Escriba su consulta aquí...");
         if ($form->isValid()) {
-            // Se obtiene un arreglo con claves "nombre", "email" y "consulta"
-            $data = $form->getData();
-            // En este punto del código se podría hacer algo con esta 
-            // información, como enviar un correo a los responsables del 
-            // evento o guardar la consulta en la base de datos o cualquier
-            // otra cosa.
+// Se obtiene un arreglo con claves "nombre", "email" y "consulta"
+// En este punto del código se podría hacer algo con esta
+// información, como enviar un correo a los responsables del
+// evento o guardar la consulta en la base de datos o cualquier
+// otra cosa.
+
+            $dia = date('d');
+            $mes = date('m');
+            $anio = date('Y');
+            $fecha = new \DateTime();
+            $fecha->setDate($anio, $mes, $dia);
+            // Aca se hidrata el objeto con los datos del formulario  y para luego persistirlo en la base de datos
+
+
+            $tarea = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            //dump($tarea['nombre']);die;
+
+            $consulta->setNombre($tarea['nombre']);
+            $consulta->setMail($tarea['email']);
+            $consulta->setFecha($fecha);
+            $consulta->setConsulta($tarea['consulta']);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($consulta);
+            $em->flush();
+
             AbstractBaseController::addInfoMessage('¡Gracias por su consulta! en breve nos podremos en contacto usted.');
             return $this->redirect($this->generateUrl('portada'));
+            //return new Response($data['nombre']);
         }
         return $this->render('@CursoSymfonyEventos/Default/consulta.html.twig'
                         , array('form' => $form->createView(),
---
+++ src/CursoSymfony/EventosBundle/Entity/Consulta.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace CursoSymfony\EventosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Consulta
+ *
+ * @ORM\Table(name="consulta")
+ * @ORM\Entity(repositoryClass="CursoSymfony\EventosBundle\Repository\ConsultaRepository")
+ */
+class Consulta
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=100)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=100)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="consulta", type="string", length=255)
+     */
+    private $consulta;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha", type="datetime")
+     */
+    private $fecha;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Consulta
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return Consulta
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set consulta
+     *
+     * @param string $consulta
+     *
+     * @return Consulta
+     */
+    public function setConsulta($consulta)
+    {
+        $this->consulta = $consulta;
+
+        return $this;
+    }
+
+    /**
+     * Get consulta
+     *
+     * @return string
+     */
+    public function getConsulta()
+    {
+        return $this->consulta;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     *
+     * @return Consulta
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+}
+
