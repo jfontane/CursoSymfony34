be233392b40e793e16a9a9c66d2415dfda73afac
jfontanellaz <jfontanellaz@gmail.com>
2020-04-13 15:55:45 -0300

    Practico 12 - Puesta a punto Correo

--- src/CursoSymfony/EventosBundle/Common/Notificacion.php
+++ src/CursoSymfony/EventosBundle/Common/Notificacion.php
@@ -29,7 +29,7 @@ class Notificacion {
             $emails['jfontanellaz@gmail.com'];
             $mensaje = Swift_Message::newInstance()
                     ->setSubject($titulo)
-                    ->setFrom(array('no-reply@eventos.local' => 'Curso Symfony'))
+                    ->setFrom(array('no_reply@escuela40.net' => 'Curso Symfony'))
                     ->setBcc($emails)
                     ->setBody($descripion);
 
